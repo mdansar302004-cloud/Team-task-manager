38e878a121da7bbc8f5319d380197257fba751bd
Mohd Ansar <mdansar302004@gmail.com>
2026-05-16 13:12:08 +0530

    fix vite preview for production

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,12 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    allowedHosts: ['all'],
+    host: '0.0.0.0',
+    port: process.env.PORT || 4173,
+  },
   server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:8000',
-        changeOrigin: true,
-      }
-    }
+    host: '0.0.0.0',
   }
 })
