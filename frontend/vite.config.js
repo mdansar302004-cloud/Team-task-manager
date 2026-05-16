7f6402fff5156398bc7066c13792c138e37a3144
Mohd Ansar <mdansar302004@gmail.com>
2026-05-16 14:48:53 +0530

    use serve instead of vite preview

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -3,12 +3,4 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  preview: {
-    allowedHosts: ['all'],
-    host: '0.0.0.0',
-    port: process.env.PORT || 4173,
-  },
-  server: {
-    host: '0.0.0.0',
-  }
 })
